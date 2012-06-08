64a893337df6e69d8db91c57c6e43ec19577ebae
Aviad Reich <avi.rei@gmail.com>
2012-06-09 01:43:17 +0300

    ready.

--- code/hw2.py
+++ code/hw2.py
@@ -126,7 +126,7 @@ def bayesianEstimator(x, t, M, alpha, sigma2):
     S = np.linalg.inv(aI + (S / sigma2))
         
     def m(xx):
-        phi_t = phi(xx).T        # vector, transose is irrlevant
+        phi_t = phi(xx).T        # vector, transpose is irrlevant
         sm = np.zeros(M+1)
         for i in range(N):
             sm += phi(x[i])*t[i]
@@ -178,11 +178,12 @@ def bag_of_words(document):
     words = chain.from_iterable(document)
     return word_feats(words)
 
-def evaluate_features(feature_extractor, N):
+def evaluate_features(feature_extractor, N, only_acc=False):
     from nltk.corpus import movie_reviews
     from nltk.classify import NaiveBayesClassifier as naive
     from nltk.classify.util import accuracy
     from nltk.metrics import precision, recall, f_measure
+    from sys import stdout
     
     negative = movie_reviews.fileids('neg')
     positive = movie_reviews.fileids('pos')
@@ -191,14 +192,13 @@ def evaluate_features(feature_extractor, N):
 
     posfeats = [(feature_extractor(movie_reviews.sents(fileids=[f])),
                  'pos') for f in positive]
-
     negtrain, negtest = stratifiedSamples(negfeats, N)
     postrain, postest = stratifiedSamples(posfeats, N)
 
     trainfeats = negtrain + postrain
     testfeats = negtest + postest
     classifier = naive.train(trainfeats)
-
+    if only_acc: return accuracy(classifier, testfeats)
     print 'accuracy: {}'.format(accuracy(classifier, testfeats))
 
     # Precision, Recall, F-measure
@@ -217,8 +217,137 @@ def evaluate_features(feature_extractor, N):
     print 'neg precision:', precision(refsets['neg'], testsets['neg'])
     print 'neg recall:', recall(refsets['neg'], testsets['neg'])
     print 'neg F-measure:', f_measure(refsets['neg'], testsets['neg'])
-    
+    stdout.flush()
     classifier.show_most_informative_features()
     return classifier
     
+    
+def unknown_words(words, wordset):
+    return sum(word in wordset for word in words)
+
+def stopword_remover(words):
+    from nltk.corpus import stopwords
+    stopset = set(stopwords.words('english'))
+    return dict([(word, True) for word in words if word not in stopset])
+
+def make_topK_non_stop_word_extractor(K, stopset):
+    import nltk
+    from nltk.corpus import movie_reviews
+    all_words = movie_reviews.words()
+    unstopabble_words = [word for word in all_words if word not in stopset]
+    all_words_fd = nltk.FreqDist(unstopabble_words)
+    it = all_words_fd.iterkeys()
+    K_words = set([it.next() for iteration in range(K)]) # FreqDist is ordered by frequency..
+    # sanity check:
+    if K == len(K_words):
+        print 'K is OK ;]'
+    else:
+        print 'we got {} instead of {} most frequent words used.'.format(len(K_words), K)
+        return
+        
+    def K_non_stop_word_feats(words, K_words, stopset):
+        return dict([(word, True) for word in words if
+                     word in K_words and not word in stopset])
+
+    def K_non_stop_bag_of_words(document):
+        from itertools import chain
+        words = chain.from_iterable(document)
+        return K_non_stop_word_feats(words, K_words, stopset)
+            
+    return K_non_stop_bag_of_words
+
+def make_pos_extractor(tags):
+    import nltk
+    from cPickle import load
+    
+    tagset = set(tags)
+    # trn = nltk.corpus.brown.tagged_sents(simplify_tags=True)
+    # t0 = nltk.DefaultTagger('NN')
+    # t1 = nltk.UnigramTagger(trn, backoff=t0)
+    # t2 = nltk.BigramTagger(trn, backoff=t1)    
+    # dump(t2, file('code/t2.pkl', 'wb'), protocol=2)
+    t2 = load(file('code/t2.pkl', 'rb'))
+
+    def _tag(sent):
+        """
+        This function is taken from hw1.
+        This function returns the inputed 'sent' as tagged by nltk.pos_tag
+        converted to Brown simplified tags.
+        """
+        # from nltk.tag.simplify import simplify_brown_tag
+        tagged_sent = t2.tag(sent)
+        # simplified = [(word, simplify_brown_tag(tag)) for
+        #               word, tag in tagged_sent]
+        return tagged_sent # simplified
+
+    def _word_feats(tagged_words, tagset):
+        return dict([(word, True) for (word, tag) in tagged_words
+                     if tag in tagset])
+
+    def _POS_bag_of_words(document):
+        from itertools import chain
+        tagged_sents = [_tag(sent) for sent in document]
+        tagged_words = list(chain.from_iterable(tagged_sents))
+        return _word_feats(tagged_words, tagset)
+
+    return _POS_bag_of_words
+
+    
+def bigram_extractor(document):
+    """
+    simple bigram extractor
+    """
+    from nltk import bigrams
+    return dict([(bigram, True) for sent in document for
+                 bigram in bigrams(sent)])
+    
+def uni_and_bigram_extractor(document):
+    from nltk import bigrams
+    unis = [(word, True) for sent in document for
+            word in sent]
+    bigs = [(bigram, True) for sent in document for
+            bigram in bigrams(sent)]
+
+    return dict(unis + bigs)  
+
+
+def make_strong_bigrams_extractor(train, n):
+
+    def strong_bigrams(words, n):
+        from nltk.collocations import BigramCollocationFinder
+        from nltk.metrics import BigramAssocMeasures
+
+        score = BigramAssocMeasures.chi_sq  # chi square measure of strength
+        bigram_finder = BigramCollocationFinder.from_words(words)
+        bigrams = bigram_finder.nbest(score, n)
+        return bigrams # [bigram for bigram in chain(words, bigrams)]
+
+    strongset = set(strong_bigrams(train, n))
+
+    def strong_extractor(document):
+        from nltk import bigrams
+        return dict([(bigram, True) for sent in document
+                     for bigram in bigrams(sent)
+                     if bigram in strongset])
+        
+    return strong_extractor
+
+def document_strong_extractor(document):
+    from itertools import chain
+    from nltk import bigrams
+    
+    def strong_bigrams(words, n):
+        from nltk.collocations import BigramCollocationFinder
+        from nltk.metrics import BigramAssocMeasures
+
+        score = BigramAssocMeasures.chi_sq  # chi square measure of strength
+        bigram_finder = BigramCollocationFinder.from_words(words)
+        bigrams = bigram_finder.nbest(score, n)
+        return bigrams
+
+    strongset = set(strong_bigrams(chain.from_iterable(document), 400))
+
+    return dict([(bigram, True) for sent in document
+                for bigram in bigrams(sent)
+                if bigram in strongset])
     
